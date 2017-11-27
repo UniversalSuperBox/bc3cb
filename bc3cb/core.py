12a9b213abefe9bc6e84f07f088bd68b0d2927bc
Dalton Durst <me@daltondurst.com>
2017-11-27 15:27:55 -0600

    Change license notice, catch errors caused by incorrect command input

--- bc3cb/core.py
+++ bc3cb/core.py
@@ -1,20 +1,18 @@
 """The nuts and bolts of bc3cb"""
 
-'''
-Copyright 2017 Dalton Durst
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-'''
+# Copyright 2017 Dalton Durst
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import shlex
 import requests
@@ -44,19 +42,34 @@ class basecampbot:
         :param request: The blob of json that Basecamp POSTS to the webapp, parsed into a dictionary
         """
 
+        self.__logger__.debug(request)
+
         commandinfo = request
-        commandline = shlex.split(commandinfo['command'])
+        user_name = commandinfo['creator']['name']
+        user_command = commandinfo['command']
+
+        # Catch any errors in the shlex string
+        try:
+            commandline = shlex.split(commandinfo['command'])
+        except ValueError as error:
+            # Something is incorrect in the user's command string
+            self.__logger__.error(' '.join([user_name, 'caused:', error.args[0],
+                                            'with the command:', user_command]))
+            errordescription = ' '.join(["⚠️Error: Please check the format of your command.",
+                                         error.args[0]])
+            self.respond(finalresponse, commandinfo['callback_url'])
+            return
+
         userfunc_torun = commandline[0]
 
-        self.__logger__.debug(commandinfo)
 
+        # Catch generic Exception so that we always reply to the user.
         try:
             usercommandresponse = self.executeuserfunction(userfunc_torun, commandline, commandinfo)
         except Exception as error:
             # Build our logging string
-            user_name = commandinfo['creator']['name']
-            user_command = commandinfo['command']
-            self.__logger__.error(' '.join(['"', user_name, '"', 'caused:', error.args[0],
+
+            self.__logger__.error(' '.join([user_name, 'caused:', error.args[0],
                                             'with the command:', user_command]))
 
             try:
@@ -66,8 +79,7 @@ class basecampbot:
                                     "For more information, contact the bot author.")
 
             # Make response
-            finalresponse = " ".join(["<details><summary>Error:", errordescription,
-                                      "</summary>", error.args[0], "</details>"])
+            finalresponse = " ".join(["⚠️Error:", errordescription])
         else:
             finalresponse = usercommandresponse
 
@@ -84,7 +96,8 @@ class basecampbot:
         else:
             raise Exception('BC3CBCommandNotFound', 'Command not found. Maybe try "help"?')
 
-    def respond(self, response, callback_url):
+    @classmethod
+    def respond(cls, response, callback_url):
         """POSTs our response back to Basecamp.
 
         :param response: Basecamp-HTML formatted string to send
