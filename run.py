fa95b4e62cf4ae2c0ef832242bea66b8a65178af
Dalton Durst <me@daltondurst.com>
2017-11-27 15:26:59 -0600

    Add receiver inside module

--- run.py
+++ run.py
@@ -1,34 +1,29 @@
-'''
-Copyright 2017 Dalton Durst
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-'''
+# Copyright 2017 Dalton Durst
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import logging
 from logging.config import fileConfig
-from threading import Thread
 import os
 import configparser
 import bc3cb
-from bc3cb import core
-import bottle
-from bottle import request, run
+from bc3cb import receiver
 
 if os.path.isfile('bc3cb.ini'):
     logging.config.fileConfig('bc3cb.ini', disable_existing_loggers=False)
     logger = logging.getLogger('bc3cbCore')
 
-MY_BOT = bc3cb.core.basecampbot(logger)
+MY_BOT = bc3cb.basecampbot(logger)
 
 # Add commands here
 
@@ -56,22 +51,6 @@ def ping(commandline, commandinfo):
 
     return '<b>pong</b>'
 
-
-#Set up receiver
-
-@bottle.post('/basecamp3receiver')
-def call_core():
-    """The receiver function that kicks off the rest of bc3cb"""
-
-    if not request.json:
-        return bottle.HTTPResponse(status=400, body="Missing command")
-
-    requestdata = request.json
-
-    bot_thread = Thread(target=MY_BOT.commandworker, args=(requestdata,))
-    bot_thread.start()
-
-    return bottle.HTTPResponse(status=204)
-
 if __name__ == '__main__':
-    run(debug=False)
+    bc3cb.receiver.BOT_INSTANCE = MY_BOT
+    bc3cb.receiver.run(debug=False)
